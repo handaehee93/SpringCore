0307accf05621aa4d69e597900f9ca99575aba22
HanDaeHee <hdh89900@gmail.com>
2024-02-27 17:06:39 +0900

    주문과 할인 도메인 테스트

---
+++ SpringCore/src/test/java/inflearn/SpringCore/order/OrderServiceTest.java
@@ -0,0 +1,22 @@
+package inflearn.SpringCore.order;
+
+import inflearn.SpringCore.member.Grade;
+import inflearn.SpringCore.member.Member;
+import inflearn.SpringCore.member.MemberService;
+import inflearn.SpringCore.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
