e13accc06558d779330d2bb41e1672d9ea78f284
HanDaeHee <hdh89900@gmail.com>
2024-02-28 16:26:29 +0900

    역할과 구현을 명확히 한 AppConfig 리팩토링

--- SpringCore/src/main/java/inflearn/SpringCore/AppConfig.java
+++ SpringCore/src/main/java/inflearn/SpringCore/AppConfig.java
@@ -10,9 +10,8 @@ import inflearn.SpringCore.order.OrderService;
 import inflearn.SpringCore.order.OrderServiceImpl;
 
 public class AppConfig {
-    // 각각의 인터페이스를 반환 타입으로 하는 메서드를 만들고 반환 값으로 구현체에 인스턴스를 전달한다.
-    // 이렇게 하면 구현체에서는 인터페이스에만 의존하게 되고, AppConfig에서 전달 받은 인스턴스를 통해
-    // 인터페이스의 구현체를 사용하게 된다.
+
+    // 메소드 명이 역할, 반환 값이 구현
     public MemberService memberService () {
         return new MemberServiceImpl(memberRepository());
     }
