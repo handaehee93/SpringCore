238935f05587e907be943d7b0532b54841d8fc24
handaehee93 <hdh8990@naver.com>
2024-03-01 16:55:47 +0900

    git clone 테스트

--- SpringCore/src/main/java/inflearn/SpringCore/SpringCoreApplication.java
+++ SpringCore/src/main/java/inflearn/SpringCore/SpringCoreApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringCoreApplication {
-
+	// git clone 테스트
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreApplication.class, args);
 	}
